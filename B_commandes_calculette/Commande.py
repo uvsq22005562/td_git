8fd39274f54565094c9655b7f47cce7c299428af
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-12-16 16:43:27 +0100

    Commande

---
+++ B_commandes_calculette/Commande.py
@@ -0,0 +1,32 @@
+import math
+
+
+def somme(A, B):
+    """ Fais la somme de 2 nombre """
+    A + B = C
+    return C
+
+
+def divide(A, B):
+    """ Fais la division de 2 nombres """
+    A / B = C
+    return C
+
+
+def multiplie(A, B):
+    """ fais la multiplication de 2 nombres"""
+    A * B = C
+    return C
+
+
+def power(A, B):
+    """ fais la puissance de 2 nombres """
+    A ** B = C
+    return C
+
+
+def soustraction(A, B):
+    """ fais la soustraction de 2 nombres """
+    A - B = C
+    return C
+
